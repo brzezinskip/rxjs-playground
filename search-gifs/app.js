cf16ae3c2ebc314d12db31c6f19982f4aaa5cd40
Piotr Brzezinski <peb.brzezinski@gmail.com>
2016-12-27 15:45:00 +0100

    keup event listener

--- search-gifs/app.js
+++ search-gifs/app.js
@@ -1,1 +1,19 @@
-var input = document.getElementsByClassName('search-input');
+const input = document.getElementsByClassName('search-input');
+
+
+// observable listening to the keup event
+const keyups = Rx.Observable.fromEvent(input, 'keyup')
+    .pluck('target', 'value')
+    .filter(text => text.length > 2);
+
+// debounce input for 500ms
+const debounced = keyups
+    .debounce(500);
+
+// get only distinct values to eliminate control characters
+const distinct = debounced
+    .distinctUntilChanged();
+
+
+// querying giphy.com
+
